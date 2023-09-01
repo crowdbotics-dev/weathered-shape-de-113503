8d817c0a25803590df610295cffa3273cffd286e
Crowdbotics <engineering@crowdbotics.com>
2023-09-01 15:16:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-shape-de-113503",
+  url: "https://weathered-shape-de-113503.botics.co",
+  api: "https://weathered-shape-de-113503.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
